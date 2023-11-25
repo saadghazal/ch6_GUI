c3b0ae1b741cb1f70c0b1c0ccaff2ffa0336a584
saadghazal <saad.m.ghazal@icloud.com>
2023-11-25 16:14:32 +0300

    Creating Content Pane with adding a label to it

--- src/JFrame1.java
+++ src/JFrame1.java
@@ -1,4 +1,6 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+
 public class JFrame1 extends JFrame {
 
     public JFrame1(){
@@ -8,7 +10,12 @@ public class JFrame1 extends JFrame {
         setLocation(600,250); //set location of where will jframe will be
         setVisible(true); // show jframe on screen
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // default behavior of exiting jframe
+        Container jFrameContent = getContentPane(); // getting the empty container
+        jFrameContent.setLayout(new GridLayout(5,2)); // setting gridlayout with 5 rows and 2 columns
+        JLabel label1 = new JLabel("Your Name"); // creating JLabel Object with title Your Name
+        jFrameContent.add(label1);// adding JLabel object that we created to the container
 
+        setContentPane(jFrameContent); // setting the new modification to the JFrame Container
     }
 
     public static void main(String[] args) {
