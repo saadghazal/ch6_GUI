03334033235b14a27a2ac95f0c98788887ffd593
saadghazal <saad.m.ghazal@icloud.com>
2023-11-25 16:21:04 +0300

    Creating Aligned Labels

--- src/JFrame1.java
+++ src/JFrame1.java
@@ -14,6 +14,10 @@ public class JFrame1 extends JFrame {
         jFrameContent.setLayout(new GridLayout(5,2)); // setting gridlayout with 5 rows and 2 columns
         JLabel label1 = new JLabel("Your Name"); // creating JLabel Object with title Your Name
         jFrameContent.add(label1);// adding JLabel object that we created to the container
+        JLabel label2 = new JLabel("Right Aligned Label",SwingConstants.RIGHT); //Creating  Right Aligned Label
+        jFrameContent.add(label2); // Adding The Right Aligned Label
+        JLabel label3 = new JLabel("Center Aligned Label",SwingConstants.CENTER); // Creating The Center Aligned Label
+        jFrameContent.add(label3); // Adding The Center Aligned Label
 
         setContentPane(jFrameContent); // setting the new modification to the JFrame Container
     }
