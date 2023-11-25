a9a73618c08beda3d549e0c89d9207e9c8b41a73
saadghazal <saad.m.ghazal@icloud.com>
2023-11-25 16:36:30 +0300

    Creating JButton

--- src/JFrame1.java
+++ src/JFrame1.java
@@ -6,7 +6,7 @@ public class JFrame1 extends JFrame {
     public JFrame1(){
 
         setTitle("GUI Chapter"); // set title of jframe
-        setSize(400,400); // set size of jframe
+        setSize(700,400); // set size of jframe
         setLocation(600,250); //set location of where will jframe will be
         setVisible(true); // show jframe on screen
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // default behavior of exiting jframe
@@ -23,7 +23,9 @@ public class JFrame1 extends JFrame {
 
         jFrameContent.add(newTextField);
 
+        JButton newButton = new JButton("Python Button"); // Creating JButton With Title
 
+        jFrameContent.add(newButton); // adding the button to the container
 
 
         setContentPane(jFrameContent); // setting the new modification to the JFrame Container
